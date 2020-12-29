06e95fd4962952593b91ba729c9792a53bc4e840
Lionkk <Lion_yan@outlook.com>
2020-12-29 17:41:11 +0800

    更新 octopus.ts

--- octopus.ts
+++ octopus.ts
@@ -4,5 +4,15 @@
 //% color=#00B1ED  icon="\uf005" block="Octopus_Basic" blockId="Octopus_Basic"
 //% groups='["Digital", "Analog", "IIC Port"]'
 namespace Octopus_Basic {
+    /**
+    * TODO: Get the value of the potentiometer(0~1023)
+    * @param Rjpin AnalogPin, eg: AnalogPin.P1
+    */
+    //% blockId="potentiometer" block="Trimpot %Rjpin analog value"
+    //% subcategory=Input color=#E2C438 group="Analog"
+    export function trimpot(Rjpin: AnalogPin): number {
+        return pins.analogReadPin(Rjpin)
+    }
     
+
 }

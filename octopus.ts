95a0c7cb3ab0ae40520e28006e919553e44c076e
Lionkk <Lion_yan@outlook.com>
2020-12-29 17:36:55 +0800

    更新 pxt.json, octopus.ts

---
+++ octopus.ts
@@ -0,0 +1,8 @@
+/**
+* Functions to Octopus sensor by ELECFREAKS Co.,Ltd.
+*/
+//% color=#00B1ED  icon="\uf005" block="Octopus_Basic" blockId="Octopus_Basic"
+//% groups='["Digital", "Analog", "IIC Port"]'
+namespace Octopus_Basic {
+    
+}
